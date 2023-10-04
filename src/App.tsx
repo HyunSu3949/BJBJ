88c38366a149ed52e208559d2281c0a05e4cf877
hyunsu.An <ahs39490215@gmail.com>
2023-10-05 00:00:02 +0900

    feat: msw 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,32 @@
+import { useState, useEffect } from 'react';
+type ApiResponse = {
+  code: number;
+  message: string;
+  data: {
+    totalCount: number;
+    clubList: Club[];
+  };
+};
+
+type Club = {
+  id: string;
+  title: string;
+  contents: string;
+  imgUrl: string;
+  tags: string;
+  likes: number;
+};
+
 export function App() {
+  const [data, setData] = useState<ApiResponse>();
+  useEffect(() => {
+    fetch('/clubs/likes', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.data);
+      });
+  }, []);
   return <div>Hi</div>;
 }
