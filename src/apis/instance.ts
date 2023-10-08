500425ff6a6cdbc7d750b840fa1b473286873294
hyunsu.An <ahs39490215@gmail.com>
2023-10-08 22:48:54 +0900

    feat: axios instance 생성

---
+++ src/apis/instance.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+let baseURL;
+if (process.env.NODE_ENV === 'development') {
+  baseURL = process.env.DEV_DOMAIN;
+} else {
+  baseURL = process.env.PROD_DOMAIN;
+}
+
+const axiosInstance = axios.create({
+  baseURL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (res: AxiosResponse) => res,
+  (error: AxiosError<unknown>) => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
