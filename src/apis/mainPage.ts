f00807531943dfd28bf8af2bbdbc78bcb0e92987
hyunsu.An <ahs39490215@gmail.com>
2023-10-09 16:29:16 +0900

    feat: mainpage api 함수 이름 변경및 관련 파일 수정

--- src/apis/mainPage.ts
+++ src/apis/mainPage.ts
@@ -1,17 +1,7 @@
 import axiosInstance from './instance';
 
-export async function getClubsSortedByLikes(sortBy: 'likes' | 'createdAt') {
+export async function getClubsSortedBy(sortBy: 'likes' | 'createdAt') {
   const res = await axiosInstance.get(`/clubs?sortby=${sortBy}`);
   const response = res.data.data;
   return response;
-}
-
-export async function likesClub({
-  clubId,
-  userId,
-}: {
-  clubId: string;
-  userId: string;
-}) {
-  await axiosInstance.post('/likeclubs', { clubId, userId });
 }
