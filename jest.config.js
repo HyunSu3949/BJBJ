f029a19da5c1c6f56788305a895171d6a07ecb41
hyunsu.An <ahs39490215@gmail.com>
2023-10-13 00:02:06 +0900

    docs: jest에서 svg컴포넌트 렌더하기위한 라이브러리 설치및 설정

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
 };
