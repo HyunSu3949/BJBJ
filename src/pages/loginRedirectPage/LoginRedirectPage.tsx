5a7f173e20e1fbf2587800f7b71026edb493ef97
hyunsu.An <112445899+HyunSu3949@users.noreply.github.com>
2023-10-15 21:59:35 +0900

    Update LoginRedirectPage.tsx

--- src/pages/loginRedirectPage/LoginRedirectPage.tsx
+++ src/pages/loginRedirectPage/LoginRedirectPage.tsx
@@ -5,10 +5,12 @@ export default function LoginRedirectPage() {
   const navigate = useNavigate();
 
   const handleLogin = () => {
+    console.log('handleLogin');
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const Access_Token = urlParams.get('Access_Token');
     const Refresh_Toke = urlParams.get('Refresh_Toke');
+    console.log(Access_Token);
     if (Access_Token) localStorage.setItem('Access_Token', Access_Token);
     if (Refresh_Toke) localStorage.setItem('Refresh_Token', Refresh_Toke);
   };
