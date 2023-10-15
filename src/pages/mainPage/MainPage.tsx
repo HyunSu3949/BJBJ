170abee81f9e7c40981e20ee91d9eb4d28cddba1
hyunsu.An <ahs39490215@gmail.com>
2023-10-15 21:08:32 +0900

    토큰 저장

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,31 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ClubList from './../../components/mainPageComponents/clubList/ClubList';
 import FeedList from '../../components/mainPageComponents/feedList/FeedList';
 import MoreButton from './../../components/mainPageComponents/moreButton/MoreButton';
 
 export default function MainPage() {
+  const [accessToken, setAccessToken] = useState<null | string>(null);
+  const [refreshToken, setRefreshToken] = useState<null | string>(null);
+
+  const handleLogin = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    setAccessToken(urlParams.get('Access_Token'));
+    setRefreshToken(urlParams.get('Refresh_Token'));
+  };
   useEffect(() => {
-    console.log('mainPage mount');
-
-    const handleLogin = () => {
-      const queryString = window.location.search;
-      console.log(queryString);
-
-      const urlParams = new URLSearchParams(queryString);
-      console.log(urlParams);
-      for (const [key, value] of urlParams.entries()) {
-        console.log(`${key}, ${value}`);
-      }
-      const accessToken = urlParams.get('Access_Token');
-      console.log('token: ', accessToken);
-      const refreshToken = urlParams.get('Refresh_Token');
-      if (accessToken !== null)
-        localStorage.setItem('Access_Token', accessToken.slice(8));
-      if (refreshToken !== null)
-        localStorage.setItem('Refresh_Token', refreshToken.slice(8));
-    };
-    handleLogin();
-  }, []);
+    if (!accessToken) handleLogin();
+  }, [accessToken]);
   return (
     <>
       <h1>메인페이지</h1>
