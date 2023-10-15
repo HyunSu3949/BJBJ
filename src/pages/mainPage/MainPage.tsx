0b14262c476507f79dd1997f0ccda99cd0994da9
hyunsu.An <112445899+HyunSu3949@users.noreply.github.com>
2023-10-15 12:36:35 +0000

    Update MainPage.tsx

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,24 +1,8 @@
-import React, { useEffect } from 'react';
 import ClubList from './../../components/mainPageComponents/clubList/ClubList';
 import FeedList from '../../components/mainPageComponents/feedList/FeedList';
 import MoreButton from './../../components/mainPageComponents/moreButton/MoreButton';
 
 export default function MainPage() {
-  const handleLogin = () => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    window.localStorage.setItem(
-      'Access_Token',
-      urlParams.get('Access_Token') || '',
-    );
-    window.localStorage.setItem(
-      'Refresh_Token',
-      urlParams.get('Refresh_Token') || '',
-    );
-  };
-  useEffect(() => {
-    handleLogin();
-  }, []);
   return (
     <>
       <h1>메인페이지</h1>
