6f72f92b8f8f28eaf8454a976e96befb29651159
hyunsu.An <ahs39490215@gmail.com>
2023-10-15 19:03:27 +0900

    feat: login log 추가

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -5,11 +5,17 @@ import MoreButton from './../../components/mainPageComponents/moreButton/MoreBut
 
 export default function MainPage() {
   useEffect(() => {
+    console.log('mainPage mount');
+
     const handleLogin = () => {
       const queryString = window.location.search;
+      console.log(queryString);
+
       const urlParams = new URLSearchParams(queryString);
+      console.log(urlParams);
       const Access_Token = urlParams.get('Access_Token');
-      const Refresh_Toke = urlParams.get('Refresh_Toke');
+      console.log(Access_Token);
+      const Refresh_Toke = urlParams.get('Refresh_Token');
       if (Access_Token) localStorage.setItem('Access_Token', Access_Token);
       if (Refresh_Toke) localStorage.setItem('Refresh_Token', Refresh_Toke);
     };
