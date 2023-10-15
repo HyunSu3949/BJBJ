2685ff164d40bd1a307251b82f14041d90218893
hyunsu.An <112445899+HyunSu3949@users.noreply.github.com>
2023-10-15 12:42:03 +0000

    fix: navbar에서 localStorage설정 삭제

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,22 +1,6 @@
-import React, { useEffect } from 'react';
 import { domains } from '../../constants/constants';
 
 export default function NavBar() {
-  useEffect(() => {
-    const handleTokenMessage = (event: MessageEvent) => {
-      if (event.origin !== `${domains.frontEnd}`) {
-        return;
-      }
-
-      const Access_Token = event.data.Access_Token;
-      const Refresh_Toke = event.data.Refresh_Toke;
-      localStorage.setItem('Access_Token', Access_Token);
-      localStorage.setItem('Refresh_Token', Refresh_Toke);
-    };
-
-    window.addEventListener('message', handleTokenMessage);
-  }, []);
-
   return (
     <div>
       <a href={domains.backEnd + '/oauth2/authorization/google'}>로그인</a>
