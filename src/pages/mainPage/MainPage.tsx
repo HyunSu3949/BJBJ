e6457e9daf8d528694791333d911e2de7e889dc4
hyunsu.An <ahs39490215@gmail.com>
2023-10-16 19:28:12 +0900

    feat: token 처리 함수 추가

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,8 +1,15 @@
 import ClubList from './../../components/mainPageComponents/clubList/ClubList';
 import FeedList from '../../components/mainPageComponents/feedList/FeedList';
 import MoreButton from './../../components/mainPageComponents/moreButton/MoreButton';
+import { useEffect } from 'react';
+import { useUserContext } from '../../components/contexts/userContext';
 
 export default function MainPage() {
+  const { afterGetToken } = useUserContext();
+  useEffect(() => {
+    const queryString = window.location.search;
+    if (queryString) afterGetToken(queryString);
+  }, []);
   return (
     <>
       <h1>메인페이지</h1>
@@ -13,7 +20,7 @@ export default function MainPage() {
         <MoreButton route="/search" />
       </section>
       <section>
-        <h2>v피드 목록</h2>
+        <h2>피드 목록</h2>
         <FeedList sortBy="likes" />
         <MoreButton route="/search" />
       </section>
