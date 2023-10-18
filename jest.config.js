b1ba475b3392f2f2d0fa250eba72c8af16cec5c4
hyunsu.An <ahs39490215@gmail.com>
2023-10-18 23:40:52 +0900

    fix: 파일위치 수정

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
     '^.+\\.svg$': 'jest-transformer-svg',
   },
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/fileMock.js',
   },
 };
