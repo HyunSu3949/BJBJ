3f4c659455dc96c3c39f8b5cda1ecb1043aab469
hyunsu.An <ahs39490215@gmail.com>
2023-10-22 19:59:31 +0900

    style:

--- src/components/clubDetailPageComponents/likeButton/useLikeClub.tsx
+++ src/components/clubDetailPageComponents/likeButton/useLikeClub.tsx
@@ -11,7 +11,7 @@ export default function useLikeClub({ clubId }: { clubId: string }) {
     } else {
       setIsLike(false);
     }
-  }, [likedClubs.length]);
+  }, [clubId, likedClubs, likedClubs.length]);
 
   const onClickForLike = async () => {
     await likesClub({ clubId, userId: userProfile.userId });
