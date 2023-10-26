81001f28138fbd0f6c37785f6a71a9f6c2e922a1
hyunsu.An <ahs39490215@gmail.com>
2023-10-26 19:03:48 +0900

    ip주소 확인

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,8 +1,12 @@
 import ClubList from './../../components/mainPageComponents/clubList/ClubList';
 import FeedList from '../../components/mainPageComponents/feedList/FeedList';
 import MoreButton from './../../components/mainPageComponents/moreButton/MoreButton';
+import axios from 'axios';
 
 export default function MainPage() {
+  axios.get('https://api.ipify.org?format=json').then(response => {
+    console.log(response.data.ip);
+  });
   return (
     <>
       <h1>메인페이지</h1>
