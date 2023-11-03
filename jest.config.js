737dabb0fd6f99a20d25cadcd4b313880223aacc
hyunsu.An <ahs39490215@gmail.com>
2023-11-03 16:29:23 +0900

    docs: testing에서 css 파일 인식을 위한 라이브러리 설치

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
 };
